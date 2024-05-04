dbccb94b9d5512877b4b14b0be72a5fd657de81b
unknown <sheypol123@gmail.com>
2024-05-04 17:42:55 -0600

    mvc

---
+++ controller/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion=mysqli_connect("129.121.5.164","detponco_test","Test33247456","detponco_drover")or die(
+    "error de conexion");
+?>
---
+++ controller/validar.php
@@ -0,0 +1,27 @@
+<?php
+include('db.php');
+$correo=$_POST['CORREO'];
+$contrasenia=$_POST['CONTRASENIA'];
+
+
+
+$consulta="SELECT*FROM USUARIO where CORREO='$correo' and CONTRASENIA='$contrasenia'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:index.html");
+
+}else{
+    ?>
+    <?php
+    include("login.html");
+
+  ?>
+  <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
+  <?php
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
